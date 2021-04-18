c304e1ff55b1fb269b1f31dac76557a724b30e74
Blake <xblak@users.noreply.github.com>
2021-04-18 12:01:25 -0700

    IS_LAYER_OFF doesn't work

--- keymap.c
+++ keymap.c
@@ -3,25 +3,27 @@ enum custom_keycodes {          // Make sure have the awesome keycode ready
   ALT_TAB = SAFE_RANGE,
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    if (is_alt_tab_active) {
+        unregister_code(KC_LALT);
+        is_alt_tab_active = false;
+    }
+    return state;
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) { // This will do most of the grunt work with the keycodes.
-    case ALT_TAB:
-      if (record->event.pressed) {
-        if (!is_alt_tab_active) {
-          is_alt_tab_active = true;
-          register_code(KC_LALT);
-        }
-        register_code(KC_TAB);
-      } else {
-        unregister_code(KC_TAB);
-      }
-      break;
-  }
-  if (is_alt_tab_active) {
-      if(IS_LAYER_OFF(layer)) {
-          unregister_code(KC_LALT);
-          is_alt_tab_active = false;
-      }
-  }
-  return true;
+    switch (keycode) { // This will do most of the grunt work with the keycodes.
+        case ALT_TAB:
+            if (record->event.pressed) {
+                if (!is_alt_tab_active) {
+                  is_alt_tab_active = true;
+                  register_code(KC_LALT);
+                }
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+            }
+            break;
+    }
+    return true;
 }
